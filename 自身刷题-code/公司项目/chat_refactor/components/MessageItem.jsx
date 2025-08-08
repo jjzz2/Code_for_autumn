24f8589dbfabbe5638cd3421745d4f603aa3b833
jiangz3 <jiangz3@trip.com>
2025-08-08 15:56:45 +0800

    feat:新增修改

---
+++ 自身刷题-code/公司项目/chat_refactor/components/MessageItem.jsx
@@ -0,0 +1,47 @@
+import ChatAvatar from './ChatAvatar';
+import { appInfo as myApp } from '@thresh/thresh-component'
+import { PositionEnum } from '../../enum'
+import { RootState } from '../../store/reducers'
+const MessageItem = ({ message }) => {
+    const { assistantId } = useSelector(() => RootState.pageData);
+    //1.确定消息显示逻辑
+    if (!message.imgShow && message.base.type === 'image') {
+        return null;
+    }
+    const position = useMemo(() => {
+        if ((myApp.app.isDriver && message.fromUid !== assistantId) ||
+            (myApp.app.isShipper && message.fromUid === assistantId)) {
+            return PositionEnum.LEFT;
+        } else {
+            return PositionEnum.RIGHT;
+        }
+    }, [message.fromUid, assistantId])
+    const tipRender = (message) => {
+        return (
+            <>
+                {message.base.bidStateMsg ? (
+                    <MiddleTip card={1} prefixText={message.base.bidStateMsg} />
+                ) : null}
+                {message.base.messageTip ? (
+                    <MiddleTip card={2} prefixText={message.base.message} />
+                ) : null}
+            </>
+        )
+    }
+    return (
+        <>
+            {tipRender(message)}
+            <View
+                className={styles['chat-record-item']}
+                style={{ flexDirection: position === PositionEnum.RIGHT ? 'row-reverse' : 'row' }}>
+                <ChatAvatar
+                    className={styles['chat-record-avatar']}
+                    position={position}
+                    message={message}>
+                </ChatAvatar>
+                {/* 2.信息提示 */}
+                <MessageBubble message={message} position={position}></MessageBubble>
+            </View>
+        </>
+    )
+}
---
+++ 自身刷题-code/公司项目/chat_refactor/index.jsx
@@ -0,0 +1,9 @@
+
+const chatRefactor = () => {
+    return (
+        <div className="chat-refactor">
+            <h1>Chat Refactor</h1>
+        </div>
+    );
+};
+export default chatRefactor;
