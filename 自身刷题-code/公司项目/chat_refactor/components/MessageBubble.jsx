c9ba4dcb653a97f47dfbd8cec6b67db81891dbcc
jiangz3 <jiangz3@trip.com>
2025-08-08 17:39:54 +0800

    feat:完成20%

---
+++ 自身刷题-code/公司项目/chat_refactor/components/MessageBubble.jsx
@@ -0,0 +1,4 @@
+const MessageBubble = ({ message,positon}) => {
+    //对其不同的信息进行渲染
+}
+export default MessageBubble;
---
+++ 自身刷题-code/公司项目/chat_refactor/components/cardFactory.jsx
@@ -0,0 +1,22 @@
+import HelloCard from '../hello-card'
+import ImageCard from '../image-card'
+import MiddleTip from '../middle-tip'
+import NotesCard from '../notes-card'
+import PayDepositCard from '../pay-deposit-card'
+
+export const CardFactory = ({ message, position }) => {
+    const {base,ext}=message;
+        const tipRender = (message) => {
+        return (
+            <>
+                {message.base.bidStateMsg ? (
+                    <MiddleTip card={HelloCard} prefixText={message.base.bidStateMsg} />
+                ) : null}
+                {message.base.messageTip ? (
+                    <MiddleTip card={PayDepositCard} prefixText={message.base.message} />
+                ) : null}
+            </>
+        )
+    }
+}
+export default CardFactory;
